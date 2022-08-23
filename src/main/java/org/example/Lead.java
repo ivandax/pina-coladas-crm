42fad33d953829136c10c096b7a3f7964c059c18
Daycar <daycar.johanna99@gmail.com>
2022-08-23 22:31:29 +0200

    Create "Lead" class

--- src/main/java/org/example/Lead.java
+++ src/main/java/org/example/Lead.java
@@ -1,5 +1,30 @@
 package org.example;
 
 public class Lead {
+    private String name;
+    private int phoneNumber;
+    private String emailAdress;
+    private String companyName;
+    private int id;
 
+    private static int idCounter = 1;
+
+    public void setId() {
+        this.id = idCounter;
+        idCounter++;
+    }
+
+    public Lead(String name, int phoneNumber, String emailAdress, String companyName) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAdress = emailAdress;
+        this.companyName = companyName;
+        setId();
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
+
+
